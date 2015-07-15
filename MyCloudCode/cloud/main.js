657acff882abfce43dcbb5a9336dfdb5f798b30b
dexterlohnes <dexterlohnes@gmail.com>
2015-07-15 11:54:34 -0700

    +Added feature where we can retrieve the user's association with a group +Also, we will now create the new roles for a new group in the beforeSave function instead of the afterSave

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,18 +1,31 @@
+var userstatusForGroupImport = require('cloud/UserStatusForGroup.js');
+
 Parse.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
-Parse.Cloud.afterSave("Group", function(request) {
-
-	if (request.object.existed() === false) {
-		//This object didn't exist before this save, which means it's BRAND SPANKIN NEW!
-		//And so we...
-		CreateRolesForNewGroup(request.object, request.user);
+Parse.Cloud.beforeSave("Group", function(request, response) {
+	console.log("Printing object");
+	console.log(JSON.stringify(request.object, null, 4));
+	var group = request.object;
+	console.log("In before save for group with name: " + group.get("name"));
+	if (group.isNew()) {
+		console.log("Group is new so going to create roles for new group");
+		CreateRolesForNewGroup(group, request.user, request).then(
+			function(hashString){
+			console.log("Retreived hashstring: " + hashString);
+			group.set("groupHashId", hashString);
+			console.log("We created them so responding with success");
+			response.success(group);
+		});
+	}else{
+		response.success(group);
 	}
 });
 
+
 Parse.Cloud.afterSave(Parse.User, function(request) {
-	if(request.object.existed() === false){
+	if (request.object.existed() === false) {
 		//Brand new User. Let's set their ACL!
 		var acl = new Parse.ACL();
 		acl.setPublicReadAccess(true);
@@ -22,62 +35,128 @@ Parse.Cloud.afterSave(Parse.User, function(request) {
 	}
 });
 
-/*
- * This will give us one of 5 responses depending on the user's status within the group
- * 
- * returns 1 - If User is a Member
- * returns 2 - If User is an Admin
- * returns 3 - If User has already been invited (and not yet accepted invitation)
- * returns 4 - If User has no outstanding association (no request to join yet made, no invitation)
- * returns 5 - If User has already requested to join
- */
-Parse.Cloud.define("getUserStatusForGroup", function(request, response) {
-	console.log("Group id: " + request.params.group);
-	response.success(2);
-});
 
-function CreateRolesForNewGroup(newGroup, user) {
+function AddUserAsMemberAndAdminToGroup(group, user) {
+
+}
+
+function AddUserAsMemberToGroup(group, user) {
+
+}
+
+function AddUserAsAdminToGroup(group, user, addToRole) {
+
+	//addToRole defaults to true
+	 addToRole = typeof addToRole !== 'undefined' ? addToRole : true;
+
+	//Use a double negative here because by default we will want to 
+	if(addToRole){
+
+	}
+	//var queryAdminRole
+
+	
+}
+
+
+/*
+ * 	AddUserToGroupRole
+ *
+ *	@param group The object id of the group we are to add the user to
+ *  @param user The Parse.User which we are to add to the group
+ *
+ *	@return A Parse.Promise once the task has been completed
+ *  
+ *	Adds the user to the Parse.Role which has admin or member (or other?) permissions for the group in question
+ */
+function AddUserToGroupRole(group, user, roleName){
+	// If the group is brand new, then the role doesn't exist yet. 
+	// We don't need to query to find that out
+
+	console.log("Adding user to group " + group.id + " for rolename " + roleName);
+
+	var queryRole = new Parse.Query(Parse.Role);
+	queryRole.equalTo("name", roleName);
+
+	return queryRole.first({
+		success:function(theRole){
+			//If we didn't get the role it's because it doesn't exist yet, so let's create it
+			if(!theRole){
+				console.log("The role didn't exist yet. So we're creating it");
+				var roleACL = new Parse.ACL();
+				roleACL.setPublicReadAccess(true);
+				theRole = new Parse.Role(roleName, roleACL);
+			}
+			console.log("Adding role");
+			theRole.getUsers().add(user);
+			return theRole.save();
+			// return theRole.save().then({
+			// 	success:function(theRole){
+			// 		console.log("Returning a saved role: ObjectID: " + savedRole.id + "name: " + savedRole.name);
+			// 		return Parse.Promise.as(theRole);
+			// 	}
+			// });
+	},
+	error: function(error) {
+		return Parse.Promise.error("Error hit. Code: " + error.code + ". Message: " + error.message);
+	}
+});
+}
+
+
+/* 
+ * 	CreateRolesForNewGroup
+ *
+ *	@param newGroup This is the newly created group. This function should only be called on new groups
+ */
+function CreateRolesForNewGroup(newGroup, user, request) {
 	// By specifying no write privileges for the ACL, we can ensure the role cannot be altered.
 	var roleACL = new Parse.ACL();
 	roleACL.setPublicReadAccess(true);
 
-	var adminRoleName = newGroup.id + '_admin';
-	var memberRoleName = newGroup.id + '_member';
+	var hashString = JSON.stringify(request, null, 0) + JSON.stringify(newGroup, null, 0) + JSON.stringify(user, null, 0);
+	console.log("Hashing string: " + hashString);
+	var theHash = hashCode(hashString);
+	console.log("Hash is " + theHash);
 
-	var groupAdminRole = new Parse.Role(adminRoleName, roleACL);
-	var groupMemberRole = new Parse.Role(memberRoleName, roleACL);
+	var adminRoleName = theHash + '_admin';
+	var memberRoleName = theHash + '_member';
 
-	groupAdminRole.getUsers().add(user);
-	groupMemberRole.getUsers().add(user);
+	console.log("Admin role name will be " + adminRoleName);
 
-	groupAdminRole.save().then(
-		function() { // The save was successful.
+	return AddUserToGroupRole(newGroup, user, adminRoleName).then(
+		function(adminRole) {
 			console.log("Created admin group: " + adminRoleName);
 
-			//Saving the admin was successful, so now save the Members group
-			groupMemberRole.getRoles().add(groupAdminRole);
-			groupMemberRole.save().then(
-				function() { // Saving the member role was successful, so now update the Group's ACL
+			return AddUserToGroupRole(newGroup, user, memberRoleName).then(
+				function(memberRole) {
 					console.log("Created member group: " + memberRoleName);
 
-					
+					//TODO: See if this works with an additional save
+					// memberRole.relation("roles").add(adminRole);
+
 					var groupACL = new Parse.ACL();
 					groupACL.setPublicReadAccess(true);
 					groupACL.setRoleReadAccess(adminRoleName, true);
 					groupACL.setRoleReadAccess(memberRoleName, true);
 					groupACL.setRoleWriteAccess(adminRoleName, true);
 					newGroup.setACL(groupACL);
-					newGroup.save();
-				},
-				function(error) { // The save failed.  Error is an instance of Parse.Error.
-					console.log("FAILURE to create member group: " + memberRoleName);
-					console.log("Code: " + error.code);
-					console.log("Message: " + error.message);
+
+
+
+
+					console.log("Created roles for the new group");
+
+					return Parse.Promise.as(theHash);
+
+					// return newGroup.save();
 				});
-		},
-		function(error) { // The save failed.  Error is an instance of Parse.Error.
-			console.log("FAILURE to create admin group: " + adminRoleName);
-			console.log("Code: " + error.code);
-			console.log("Message: " + error.message);
 		});
 }
+
+hashCode = function(s){
+  return s.split("").reduce(function(a,b){
+  	a=((a<<5)-a)+b.charCodeAt(0);
+  	return a&a;
+  },0);              
+};

3f8f76ec0a5fe7d468ceffed7b6d99ad39c3f061
dexterlohnes <dexterlohnes@gmail.com>
2015-09-30 12:13:29 -0700

    +Implemented notifications :) except for Alerts and Events, though there are placeholders for those

--- MyCloudCode/cloud/Controllers/Notifications.js
+++ MyCloudCode/cloud/Controllers/Notifications.js
@@ -1,30 +1,93 @@
+/***********************************************************************************************
+ ***********************************************************************************************
+ * NOTE: THIS IS NOT INTENDED TO BE CALLED BY OTHER CONTROLLERS
+ * CONTROLLERS IN THIS PATTERN SHOULD ONLY BE GETTING CALLED BY INTERFACES, NOT OTHER CONTROLLERS 
+ ***********************************************************************************************
+ ***********************************************************************************************/
+
 var PREFIX_NEW_MESSAGE = "NewMessage";
 var PREFIX_NEW_EVENT = "NewEvent";
 var PREFIX_ALERT = "Alert";
 var PREFIX_INVITATION_ACCEPTED = "InvitationAccepted";
 var PREFIX_MEMBERSHIP_REQUEST = "MembershipRequest";
+var PREFIX_INVITED_TO_GROUP = "InvitedToGroup";
 
-exports.subscribeUserToNotifChannel = function subscribeUserToNotifChannel(user, notifChannel, dontSave) {
+
+/**
+ * pushNotifToChannel
+ *
+ * Pushes a notification to a specific channel
+ * 
+ * @param notifMessage The body of the notification we want to send out
+ * @param channel The channel (as string) which we want to publish to
+ * @param androidTitle A string representing the title (top-most text) which we should display on Android notifs (doesn't apply to iOS)
+ *
+ * @return A Parse.Promise object containing nothing
+ */
+exports.pushNotifToChannel = function (notifMessage, channel, androidTitle) {
 	
-};
+	var promise = new Parse.Promise();
 
-exports.pushNotifToChannel = function (notifMessage, channel) {
+	console.log("Going to send push to \nchannel: " + channel + "\nWith message: " + notifMessage);
 
 	Parse.Push.send({
 		channels: [channel],
 		data: {
-			alert: notifMessage
+			alert: notifMessage,
+			title: androidTitle
 		}
 	}, {
 		success: function() {
 			// Push was successful
 			console.log("Push successfully sent");
 			console.log("Payload:\n" + notifMessage + "Channel:\n" + channel);
+			promise.resolve();
 		},
 		error: function(error) {
 			// Handle error
 			console.log("Push failed");
 			console.log("Payload:\n" + notifMessage + "Channel:\n" + channel);
+			promise.reject();
+		}
+	});
+};
+
+
+/**
+ * pushNotifToQuery
+ *
+ * Pushes a notification to all users within a Parse.Query
+ * 
+ * @param notifMessage The body of the notification we want to send out
+ * @param query The Parse.Query for Parse.Users which we want to send this notif to
+ * @param androidTitle A string representing the title (top-most text) which we should display on Android notifs (doesn't apply to iOS)
+ *
+ * @return A Parse.Promise object containing nothing
+ */
+exports.pushNotifToQuery = function (notifMessage, query, androidTitle) {
+	
+	var promise = new Parse.Promise();
+
+	console.log("Going to send push to \nquery: " + JSON.stringify(query, null, 4) + "\nWith message: " + notifMessage);
+
+	Parse.Push.send({
+		where: query,
+		data: {
+			alert: notifMessage,
+			title: androidTitle
+		}
+	}, {
+		success: function() {
+			// Push was successful
+			console.log("Push successfully sent");
+			console.log("Payload:\n" + notifMessage + "Query:\n" + JSON.stringify(query, null, 4));
+			promise.resolve();
+		},
+		error: function(error) {
+			// Handle error
+			console.log("Push failed");
+			console.log("Payload:\n" + notifMessage + "Query:\n" + JSON.stringify(query, null, 4));
+			promise.reject();
 		}
 	});
 };
@@ -45,6 +108,10 @@ exports.getInvitationAcceptedChannelForUser = function (user) {
 	return PREFIX_INVITATION_ACCEPTED + "_" + user.id;
 };
 
-exports.getNewMembershipRequestedChannelForGroup = function (user, group) {
+exports.getNewMembershipRequestedChannelForGroup = function (group) {
 	return PREFIX_MEMBERSHIP_REQUEST + "_" + group.id;
 };
+
+exports.getInvitedToGroupChannelForUser = function (user) {
+	return PREFIX_INVITED_TO_GROUP + "_" + user.id;
+}
--- MyCloudCode/cloud/Interfaces/NotificationsInterface.js
+++ MyCloudCode/cloud/Interfaces/NotificationsInterface.js
@@ -1,37 +1,123 @@
 var Notifications = require('cloud/Controllers/Notifications.js');
 
+/*
+ *	sendPushForUserPostedMessageToGroup
+ *	@param user A pointer to the _User who posted a new message
+ *	@param message A pointer to the Message which has been posted
+ *	@param group A pointer to a Group which we are sending a notification to
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
 exports.sendPushForUserPostedMessageToGroup = function (user, message, group) {
-	var channel = Notifications.getNewMessageChannelForGroup(group);
-	var alertMessage = user.get("displayName") + " posted a new message in group " + group.get("name");
-	Notifications.pushNotifToChannel(alertMessage, channel);
+	return user.fetch().then(function (theUser) {
+		user = theUser;
+		return group.fetch().then(function (theGroup) {	
+			var channel = Notifications.getNewMessageChannelForGroup(theGroup);
+
+			var pushQuery = new Parse.Query(Parse.Installation);
+			pushQuery.equalTo('channels', channel); // Set our channel
+
+			// Find all users who are NOT the author
+			var userQuery = new Parse.Query(Parse.User);
+			userQuery.notEqualTo("objectId", user.id);
+			
+			// Set our push query to only find Installations where the 'user' is not equal to our author User
+			pushQuery.matchesQuery('user', userQuery);
+
+			var alertMessage = message.get("body");
+			var title = user.get("displayName") + " - " + theGroup.get("name");
+			return Notifications.pushNotifToQuery(alertMessage, pushQuery, title);
+		});
+	});
 };
 
+/* 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED
+ *	sendPushForUserCreatedNewEventForGroup
+ *	@param user A pointer to the _User who posted a new event
+ *	@param eventTitle The title of the event which we are sending a notif about
+ *	@param group A pointer to a Group which we are sending a notification to
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
 exports.sendPushForUserCreatedNewEventForGroup = function (user, eventTitle, group) {
 	var channel = Notifications.getNewEventChannelForGroup(group);
 	var alertMessage = user.get("displayName") + " created a new event for the group " + group.get("name");
 	Notifications.pushNotifToChannel(alertMessage, channel);
 };
 
-exports.sendPushForUserCreatedNewEventForGroup = function (user, eventTitle, group) {
-	var channel = Notifications.getNewEventChannelForGroup(group);
-	var alertMessage = user.get("displayName") + " created a new event for the group " + group.get("name");
-	Notifications.pushNotifToChannel(alertMessage, channel);
-};
-
+/* 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED * 	UNTESTED UNIMPLEMENTED
+ *	sendPushForAlertToGroup
+ *	@param user A pointer to the _User who posted a new event
+ *	@param alert A pointer to the alert object (Message?) which has been posted
+ *	@param group A pointer to a Group which we are sending a notification to
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
 exports.sendPushForAlertToGroup = function (user, alert, group) {
 	var channel = Notifications.getAlertChannelForGroup(group);
 	var alertMessage = user.get("displayName") + ": " + alert;
 	Notifications.pushNotifToChannel(alertMessage, channel);
 };
 
+/*
+ *	sendPushForUsersInvitationAcceptedForGroup
+ *
+ *	@param user A pointer to the _User who accepted an invitation
+ *	@param group A pointer to a Group which this user has accepted an invitation for
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
 exports.sendPushForUsersInvitationAcceptedForGroup = function (user, group) {
-	var channel = Notifications.getInvitationAcceptedChannelForUser(user);
-	var alertMessage = "You have been accepted to the group \'" + group.get("name") + "\'";
-	Notifications.pushNotifToChannel(alertMessage, channel);
+	return user.fetch().then(function (theUser) {
+		user = theUser;
+		return group.fetch().then(function (theGroup) {
+			var channel = Notifications.getInvitationAcceptedChannelForUser(user);
+			var title = "Membership Accepted";
+			var alertMessage = "You have been accepted to the group \'" + theGroup.get("name") + "\'";
+			return Notifications.pushNotifToChannel(alertMessage, channel, title);
+		});
+	});
 };
 
+/*
+ *	sendPushForUserHasRequestedMembershipForGroup
+ *
+ *	@param user A pointer to the _User who accepted an invitation
+ *	@param group A pointer to a Group which this user has accepted an invitation for
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
 exports.sendPushForUserHasRequestedMembershipForGroup = function (user, group) {
-	var channel = Notifications.getNewMembershipRequestedChannelForGroup(group);
-	var alertMessage = user.get("displayName") + " has requested membership to your group \' " + group.get("name") + "\'";
-	Notifications.pushNotifToChannel(alertMessage, channel);
+	return user.fetch().then(function (theUser) {
+		user = theUser;
+		return group.fetch().then(function (theGroup) {
+			var channel = Notifications.getNewMembershipRequestedChannelForGroup(theGroup);
+			var alertMessage = user.get("displayName") + " has requested membership to your group \' " + theGroup.get("name") + "\'";
+			return Notifications.pushNotifToChannel(alertMessage, channel);
+		});
+	});
 };
+
+/*
+ *	sendPushForUserHasBeenInvitedToGroup
+ *
+ *	@param user A pointer to the _User who has been invited to 'group'
+ *	@param invitedBy A pointer to the _User who invited 'user'
+ *	@param group A pointer to a Group which 'user' has been invited to
+ *	
+ *	@return A Parse.Promise which is bound by the Push completing
+ */
+exports.sendPushForUserHasBeenInvitedToGroup = function (user, invitedBy, group) {
+	return user.fetch().then(function (theUser) {
+		user = theUser;
+		return group.fetch().then(function (theGroup) {
+			group = theGroup;
+			return invitedBy.fetch().then(function (inviter) {
+				var title = "Invited to group";
+				var channel = Notifications.getInvitedToGroupChannelForUser(user);
+				var alertMessage = inviter.get("displayName") + " has invited you to join the group \' " + group.get("name") + "\'";
+				return Notifications.pushNotifToChannel(alertMessage, channel, title);
+			});
+		});
+	});
+}
